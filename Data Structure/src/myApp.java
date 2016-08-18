00a06198356971509c6f814619e7e52166b89539
LaTresha Reed <latreshareed@me.com>
2016-08-18 16:28:34 -0400

    first commit

---
+++ Data Structure/src/myApp.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class myApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Stack <Integer> s = new Stack <Integer>();
+		
+		s.push (5);
+		s.push (15);
+		s.push (1);
+		
+		System.out.println(s.pop());
+	}
+
+}
